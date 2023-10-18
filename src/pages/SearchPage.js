a56057821301a9730be2f13e76ca8a5471968f50
Vincent Kioko <vincentkioko51@gmail.com>
2023-10-18 13:56:18 +0300

    Pages Setup

---
+++ src/pages/SearchPage.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { RiSearchLine } from 'react-icons/ri';
+import { BiX } from 'react-icons/bi';
+import { motion } from 'framer-motion';
+
+import Header from '../components/Header';
+
+const SearchPage = ({ setQuery }) => {
+    const [city, setCity] = React.useState('');
+
+    const handelResetValue = () => {
+        setCity('');
+    }
+
+    const handelLocationClick = () => {
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const { latitude, longitude } = position.coords;
+                setQuery({ lat: latitude, lon: longitude });
+            })
+        }
+    }
+
+    return (
+        <motion.div
+            className='search-container p-6'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+        >
+            <Header title="search for a city" />
+            <div className="search-input-container w-full flex items-center my-6 h-12 bg-[#1f1f21] rounded-full">
+                <RiSearchLine
+                    onClick={handelSearchClick}
+                    className="ml-4 text-xl text-gray-300"
+                />
+                <input
+                    value={city}
+                    onChange={(e) => setCity(e.currentTarget.value)}
+                    onKeyUp={(e) => handelSearchClick(e)}
+                    type="text"
+                    className="w-full h-full bg-transparent rounded-full outline-none text-sm px-3 placeholder:text-sm placeholder:text-gray-500"
+                    placeholder="Search"
+                />
+                {city ? (
+                    <BiX
+                        onClick={handelResetValue}
+                        className="mr-4 p-1 text-xl text-gray-300 cursor-pointer hover:text-white hover:scale-110 transition"
+                    />
+                ) : (
+                    ''
+                )}
+            </div>
+            <div className="w-full flex justify-center">
+                <div
+                    onClick={handelLocationClick}
+                    className="flex w-52 flex-col justify-center items-center cursor-pointer text-slate-400 my-10 hover:scale-125 transition hover:text-slate-300"
+                >
+                    <SlLocationPin className="text-6xl lg:text-8xl" />
+                    <p className="text-sm lg:text-lg mt-4">Find my location</p>
+                </div>
+            </div>
+        </motion.div>
+    )
+}
+
+export default SearchPage;
---
+++ src/pages/SettingPage.js
@@ -0,0 +1,46 @@
+import { motion } from "framer-motion";
+
+import Header from "../components/Header";
+import { formatToLocalTime } from "../services/weatherService";
+
+
+
+const SettingPage = ({ weather, setUnits, isMetric, setIsMetric }) => {
+    const handleChangeUnits = () => {
+        setIsMetric(!isMetric)
+        setUnits(isMetric ? 'metric' : 'imperial')
+    }
+
+    return (
+        <motion.div
+            className='p-6'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+        >
+            <Header title='Setting' />
+            <div className="setting-container flex flex-col items-center my-6">
+                <div className="time text-8xl mb-9">
+                    <p>
+                        {weather
+                            ? formatToLocalTime(weather.dt, weather.timezone, 'hh: mm')
+                            : ''}
+                    </p>
+                </div>
+                <div
+                    onClick={handelChangeUnits}
+                    className="w-full py-4 px-7 mb-10 flex justify-between rounded-full glassEffect"
+                >
+                    <p>Temperature based on</p>
+                    <p className="text-yellow-400">{isMetric ? 'F' : 'C'}</p>
+                </div>
+                <div className="w-full py-4 px-7 mb-10 flex justify-between rounded-full glassEffect">
+                    <p>Theme</p>
+                    <p className="text-yellow-400">Dark</p>
+                </div>
+            </div>
+        </motion.div>
+    )
+}
+
+export default SettingPage;
